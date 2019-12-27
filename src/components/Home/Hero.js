e99eead32b9c51e2afbac54899b126c42ec6e5df
wcolts2000 <wcolts2000@gmail.com>
2019-12-27 12:17:15 -0800

    moved location to hopfully load better on mobile

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import sprite from "./../../assets/svg/sprite.svg";
-import video from "../../assets/video/Explosion.mp4"
-import logo from '../../assets/svg/NNNS_Logo.svg'
 
 function Hero() {
     return (
-        <>
-        <div className="home__video-wrapper">
-            <video class="home__hero-video" width="100%" autoPlay muted>
-                <source src={video} type="video/mp4"/>
-                Sorry, your browser does not support embedded videos.
-            </video>
-            <img class="nnns-logo" src={logo} alt="Northern Nevada Network Service Logo"/>
-        </div>
         <div className="hero">
             <div className="hero__left">
                 <h1>
@@ -38,7 +28,6 @@ function Hero() {
                 </ul>
             </div>
         </div>
-        </>
     )
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Hero from "./Hero";
 import CTA from "./CTA";
+import video from "../../assets/video/Explosion.mp4"
+import logo from '../../assets/svg/NNNS_Logo.svg'
 
 function Home({ toggleModal, ...rest }) {
   return (
     <div>
       <Hero />
       <CTA toggleModal={toggleModal} {...rest} />
+      <div className="home__video-wrapper">
+            <video class="home__hero-video" width="100%" loop autoPlay muted>
+                <source src={video} type="video/mp4"/>
+                Sorry, your browser does not support embedded videos.
+            </video>
+            <img class="nnns-logo" src={logo} alt="Northern Nevada Network Service Logo"/>
+        </div>
     </div>
   );
 }
