ec763daa9b9471fd65a4712368bc96d415108e12
wcolts2000 <wcolts2000@gmail.com>
2019-12-28 23:18:11 -0800

    redoing landing page layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import SocialHeader from "./SocialHeader";
-import sprite from '../../assets/svg/sprite.svg'
+import logo from '../../assets/svg/NNNS_NavLogo.svg';
+import hamburger from '../../assets/svg/menu.svg';
+
 
 function Header() {
   return (
     <>
       <div className="fixed-header">
-        <SocialHeader />
         <header className="App-header">
           <nav className="nav">
             <NavLink exact to="/" className="home-link">
-              <svg viewBox="0 0 60 60" className="icon">
-                <use xlinkHref={`${sprite}#wifi-signal`}></use>
-              </svg>
-              HOME
+              <img src={logo} alt="NNNS logo"/>
             </NavLink>
-            <NavLink exact to="/services">
-              SERVICES
+            <NavLink exact to="/" className="menu-link">
+              <img src={hamburger} alt="menu icon"/>
             </NavLink>
-            <NavLink to="/wifi">WIFI</NavLink>
-            <NavLink to="/contact">CONTACT US</NavLink>
           </nav>
         </header>
       </div>
