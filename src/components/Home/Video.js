390c3ff9b5a4a2158215c9cf3b415fd08412e3a4
wcolts2000 <wcolts2000@gmail.com>
2019-12-31 21:49:11 -0800

    video section card added

--- src/components/Home/Video.js
+++ src/components/Home/Video.js
@@ -4,7 +4,13 @@ import data from "../../assets/video/data.mp4";
 function Video() {
   return (
     <div className="home__video-wrapper">
-        <video class="home__hero-video" width="100%" loop autoPlay muted>
+      <div className="home__video-caption">
+        <h3>Some Title Here</h3>
+        <p>Some info goes here. Make it SEO friendly adding enough keywords to make the search crawlers happy.</p>
+        <p>You may even want to think of adding some useful outside link to a large reputable site to help with backlink seo optimization.</p>
+        <button> <a href="#!">See Pricing Here</a></button>    
+      </div>
+        <video class="home__video" width="100%" loop autoPlay muted>
             <source src={data} type="video/mp4"/>
             Sorry, your browser does not support embedded videos.
         </video>
